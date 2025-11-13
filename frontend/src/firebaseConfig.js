b79d6decd279ede44cd3874cc382c1f2f3bac946
Riya Raj <riyaraj172003@gmail.com>
2025-11-13 23:09:16 +0530

    Final frontend changes and Firebase config

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -9,18 +9,17 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-// Initialize auth and provider
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-export { auth, provider };
+export { app, analytics, auth, provider };
+
 
 
 
