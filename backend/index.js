7e432c0fc0f7ad6567e37fa1e39ec04674575378
Riya Raj <133241476+riyahde@users.noreply.github.com>
2025-11-13 20:25:18 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.static("public"));
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://smart-learning-platform-1.onrender.com",
     credentials:true
 }))
 
